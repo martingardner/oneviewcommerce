767c9f28c2438cd537a631c95dcca67134746a42
martin gardner II <martingardnerii@gmail.com>
2019-09-05 19:31:55 -0500

    successful rendering of container with store in searchrow test

--- coding-test/tests/containers/searchrow.test.js
+++ coding-test/tests/containers/searchrow.test.js
@@ -1,12 +1,32 @@
 import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import { render, cleanup } from "@testing-library/react";
-import { SearchRow } from "../../src/containers/searchrow.js";
+import SearchRow from "../../src/containers/searchrow.js";
+import AppReducers from "../../src/reducers/reducers";
 
-afterEach(cleanup);
+afterEach(() => {
+  cleanup;
+});
+
+const returnReduxSetup = () => {
+  let store = createStore(AppReducers);
+  return {
+    ...render(
+      <Provider store={store}>
+        <SearchRow />
+      </Provider>
+    ),
+    store
+  };
+};
 
 describe("SearchRow container", () => {
   it("should render wtihout breaking", () => {
-    const component = render(<SearchRow />);
+    const component = returnReduxSetup();
     expect(component).toBeTruthy();
+    expect(document.querySelector("h2")).toBeTruthy();
   });
+
+  it("by default tbody should be empty", () => {});
 });
